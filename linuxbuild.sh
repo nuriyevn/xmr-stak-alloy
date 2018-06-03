79a7bc491bc7285a88a946ebdd68c49a6380d912
Nusrat Nuriyev <nuriyevn@gmail.com>
2018-06-03 20:52:32 +0300

    first commit

---
+++ linuxbuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CMAKE_PREFIX_PART='hwloc'
+
+cmake -DCUDA_ENABLE=OFF -DMICROHTTPD_ENABLE=OFF -DOpenSSL_ENABLE=OFF  -DOpenCL_ENABLE=OFF -DXMR-STAK_COMPILE=generic -DCMAKE_LINK_STATIC=ON .
+
+#cmake --build . --config Release --target install
+
+
+
+
+#set CMAKE_PREFIX_PATH=C:\xmr-stak-dep\hwloc
+#cmake.exe -G  "Visual Studio 14 2015 Win64" -T v140,host=x64 -DMICROHTTPD_ENABLE=OFF -DOpenSSL_ENABLE=OFF  -DOpenCL_ENABLE=ON -DXMR-STAK_COMPILE=generic -DCMAKE_LINK_STATIC=ON .. 
+#cmake --build . --config Release --target install
+
+
